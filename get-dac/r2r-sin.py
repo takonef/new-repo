a2754127b22e0f892712694ec0c01d560ded5ae6
takonef <takonef@gmail.com>
2025-10-09 14:03:31 +0300

    new-sin

--- get-dac/r2r-sin.py
+++ get-dac/r2r-sin.py
@@ -6,19 +6,14 @@ leds = [16, 20, 21, 25, 26, 17, 27, 22]
 amplitude = 3.2
 signal_frequency = 10
 sampling_frequency = 1000
+dynamic_range = 3.16
 
 if __name__ == "__main__":
     dac = r2r.R2R_DAC(leds, dynamic_range, True)
     try:
-
         while True:
-            try:
-                voltage = float(input("Введите напряжение в Вольтах:"))
-                dac.set_voltage(amplitude*sg.get_sin_wave_amplitude(time.time))
-                sg.wait_for_sampling_period(sampling_frequency)
-            
-            except ValueError:
-                print("Вы ввели не число. Попробуйте еще раз:")
+            dac.set_voltage(amplitude*sg.get_sin_wave_amplitude(time.time))
+            sg.wait_for_sampling_period(sampling_frequency)
     finally:
         dac.deinit()
     
